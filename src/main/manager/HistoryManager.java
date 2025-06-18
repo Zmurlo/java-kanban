6dfb061f9f44f1d1fe886466124401f81905467a
Zmurlo <95719111+Zmurlo@users.noreply.github.com>
2025-06-18 23:49:42 +0300

    Умоляю

--- src/main/manager/HistoryManager.java
+++ src/main/manager/HistoryManager.java
@@ -1,10 +1,10 @@
 package main.manager;
 
 import main.task.ask;
-
 import java.util.ArrayList;
 
 public interface HistoryManager {
     void add(ask task);
+
     ArrayList<Object> getHistory();
 }
