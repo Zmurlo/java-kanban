ffa6b43b377b1e0344be02fd9ec79cd59f03150e
Zmurlo <95719111+Zmurlo@users.noreply.github.com>
2025-06-18 23:28:32 +0300

    Вот теперь точно загружаю новый код.

--- src/main/manager/HistoryManager.java
+++ src/main/manager/HistoryManager.java
@@ -1,11 +1,9 @@
 package main.manager;
 
 import main.task.ask;
-
 import java.util.List;
 
 public interface HistoryManager {
     void add(ask task);
-    void remove(int id);
-    <Task> List<Task> getHistory();
+    <Task> List <Task> getHistory();
 }
