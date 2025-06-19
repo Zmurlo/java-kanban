a931546ee36490eb0f474d84bc90554b584f1637
Zmurlo <95719111+Zmurlo@users.noreply.github.com>
2025-06-20 01:23:32 +0300

    Add files via upload

--- src/main/manager/TaskManager.java
+++ src/main/manager/TaskManager.java
@@ -5,30 +5,11 @@ import main.task.Subtask;
 import main.task.ask;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public interface TaskManager {
     ArrayList<Object> getHistory();
     ask getTask(int id);
-    Subtask getSubtask(int id);
-    Epic getEpic(int id);
-    List <ask> getTasks();
-    List <Subtask> getSubtasks();
-
-    void updateSubtask(Subtask subtask);
-
-    void deleteSubtask(int id);
-
-    List <Epic> getEpics();
     int addNewTask(ask task);
     int addNewSubtask(Subtask subtask);
     int addNewEpic(Epic epic);
-    void updateTask(ask task);
-    void deleteTask(int id);
-
-    void updateEpic(Epic epic);
-
-    void deleteEpic(int id);
-
-    List <Subtask> getEpicSubtasks(int epicId);
 }
