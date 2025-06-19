8cdaf057904b7f00947659185d9ac834bf1057c2
Zmurlo <95719111+Zmurlo@users.noreply.github.com>
2025-06-20 01:58:23 +0300

    Add files via upload

--- src/main/task/Epic.java
+++ src/main/task/Epic.java
@@ -3,12 +3,16 @@ package main.task;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Epic extends ask {
+public class Epic extends Task {
     private final List<Integer> subtaskIds = new ArrayList<>();
 
-    public Epic(String name, String description) { super(name, description, Status.NEW); }
+    public Epic(String name, String description) {
+        super(name, description, Status.NEW);
+    }
 
-    public List<Integer> getSubtaskIds() { return subtaskIds; }
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
 
     public void addSubtask(int subtaskId) {
         subtaskIds.add(subtaskId);
