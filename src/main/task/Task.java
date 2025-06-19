bea253540f55fba77226e6ea4784d604824de892
Zmurlo <95719111+Zmurlo@users.noreply.github.com>
2025-06-20 01:56:45 +0300

    Add files via upload

--- src/main/task/Task.java
+++ src/main/task/Task.java
@@ -18,9 +18,6 @@ public class Task {
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
 
     @Override
     public boolean equals(Object o) {
