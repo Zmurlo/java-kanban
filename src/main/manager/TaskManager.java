1b15d8fdba7ccb14ec6f9ff8cb0f0ab9eafd39de
Zmurlo <95719111+Zmurlo@users.noreply.github.com>
2025-06-20 01:31:18 +0300

    Add files via upload

--- src/main/manager/TaskManager.java
+++ src/main/manager/TaskManager.java
@@ -1,15 +1,7 @@
 package main.manager;
 
-import main.task.Epic;
-import main.task.Subtask;
-import main.task.ask;
-
 import java.util.ArrayList;
 
 public interface TaskManager {
     ArrayList<Object> getHistory();
-    ask getTask(int id);
-    int addNewTask(ask task);
-    int addNewSubtask(Subtask subtask);
-    int addNewEpic(Epic epic);
 }
